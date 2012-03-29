3e3e8d840fdbbe1a051015646675ce44a0fa1a0a
mecharius <hart.wl@gmail.com>
2012-03-30 00:37:40 +0100

    Add view template and CSS

---
+++ application/views/template.php
@@ -0,0 +1,13 @@
+<html lang="EN">
+	<head>
+		<title>Grade Manager by William Hart</title>
+		<style type="text/css" href="<?php echo base_url() . "assets/css/reset.css"; ?>">
+		</style>
+	
+	</head>
+	<body>
+		
+		<?php echo $content; ?>
+	
+	</body>
+</html>
