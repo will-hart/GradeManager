f975428a6c0f9e655e40655cde0db0d5a1b567ea
mecharius <hart.wl@gmail.com>
2012-03-30 00:37:51 +0100

    Add basic view to dashboard controller for testing

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,17 +1,38 @@
 <?php if (!defined('BASEPATH')) exit ("No direct script access allowed!");
 
-
+	
 	class Dashboard extends CI_Controller {
+		
+		private $usr; // stores the user object
 		
 		public function __construct()
 		{
+			// call the parent constructor
 			parent::__construct();
+			
+			// redirect to the login page if no session exists
 			if($this->session->userdata('logged_in') === FALSE) redirect('login');
+			
+			// get the user object
+			$this->usr = Model\User::find($this->session->userdata("user_id"));
 		}
 	
 		public function index()
 		{
-			echo "Hello dashboard";
-						
+			$content = "Hello ".$this->usr->username;
+			
+			$content .= "<br/>##########################<br/>";
+			
+			$content .=  "<table>";
+			foreach($this->usr->subject() as $subj)
+			{
+				$content .=  "<tr><td>" . $subj->code . "</td><td>" . $subj->title . "</td></tr>";
+			}
+			$content .=  "</table>";
+			
+			$content .=  "<br/><br/>##########################<br/>";
+			
+			$data['content'] = $content;
+			$this->load->view('template',$data);
 		}
 	}
