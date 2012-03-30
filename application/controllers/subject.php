4178a12160e082efd5aa63d05a3ef4b459c6976d
mecharius <hart.wl@gmail.com>
2012-03-30 02:12:56 +0100

    Implement subject creation

--- application/controllers/subject.php
+++ application/controllers/subject.php
@@ -3,7 +3,27 @@
 	
 	class Subject extends CI_Controller {
 		
-		public $usr; // stores the user object
+		protected $usr; // stores the user object
+		
+		// Set up the validation rules
+		protected $validation_rules = array(
+			'code' => array (
+				'field' 		=> 		'code',
+				'label'			=> 		'Subject Code',
+				'rules'			=>		'trim|xss_clean|max_length[32]|alpha_numeric|required'
+			),
+			'title' => array (
+				'field' 		=> 		'title',
+				'label'			=> 		'Subject Title',
+				'rules'			=>		'trim|xss_clean|max_length[255]|alpha_numeric|required'
+			),
+			'notes' => array(
+				'field' 		=> 		'notes',
+				'label'			=> 		'Subject Code',
+				'rules'			=>		'trim|xss_clean|alpha_numeric'
+			),
+		);
+		
 		
 		public function __construct()
 		{
@@ -43,6 +63,55 @@
 			
 			// load the template
 			$this->load->view('template',$data);
+		}
+		
+		
+		/*
+		 * Creates a new subject
+		 */
+		public function create()
+		{
+			// set the rules
+			$this->form_validation->set_rules($this->validation_rules);
+			
+			// check if we have post data and build a subject object
+			if($this->input->post('submit'))
+			{
+				// set the data
+				$subj = new Model\Subject();
+				$subj->code = $this->input->post('code');
+				$subj->title = $this->input->post('title');
+				$subj->users_id = $this->session->userdata('user_id');
+			}
+			else 
+			{
+				$this->session->set_flashdata('notice','Unable to create a new subject, please check your link.');
+				redirect('dashboard');
+			}
+			
+			
+			// check the rules
+			if($this->form_validation->run() === TRUE)
+			{
+				
+				// attempt to save
+				if($subj->save())
+				{
+					$this->session->set_flashdata("success","New Subject Added");
+					redirect("subject/view/".Model\Subject::last_created()->id);
+				}
+				else 
+				{
+					$this->session->set_flashdata('error','Unable to save the subject! Please try again.');
+				}
+			}
+			
+			// if the rules failed then show the form with error notices
+			// and the forms populated
+			$data['subject'] = $subj;
+			$data['content'] = $this->load->view('subject/manage_single',$data,true);
+			$this->load->view('template',$data);
 			
 		}
 	}
+	
--- application/views/subject/manage_single.php
+++ application/views/subject/manage_single.php
@@ -1,9 +1,23 @@
+<?php if (validation_errors()) : ?>
+<div class="error"><?php echo validation_errors();?></div>
+<?php endif; ?>
+
+<?php
+	$title = '';
+	$code = '';
+	if (isset($subject) && !is_null($subject)) 
+	{
+		$title = $subject->title;
+		$code = $subject->code;
+	}
+?>
+
 <div class="span-24 last">
-	<?php echo form_open(site_url('subject/add')); ?>
+	<?php echo form_open(site_url('subject/create')); ?>
 		<fieldset>
 			<legend>Add New Subject</legend>
-				Subject Code <input type="text" name="code" value="" />
-				Subject Title <input type="text" name="title" value="" />
+				Subject Code <input type="text" name="code" value="<?php echo $code; ?>" />
+				Subject Title <input type="text" name="title" value="<?php echo $title; ?>" />
 			
 				<button type="submit" value="submit" name="submit">Add New</button>
 		</fieldset>
