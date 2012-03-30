08e60ac98c79887b4066bc8c36e02f5ef1310f3f
mecharius <hart.wl@gmail.com>
2012-03-30 17:22:32 +0100

    Update migrations to 7 for year level and templates

--- application/migrations/006_templates.php
+++ application/migrations/006_templates.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-class Migration_template extends CI_Migration {
+class Migration_templates extends CI_Migration {
 
 	public function up()
 	{
---
+++ application/migrations/007_yearlevel.php
@@ -0,0 +1,49 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Migration_yearlevel extends CI_Migration {
+
+	public function up()
+	{
+		$this->dbforge->add_field(array(
+			'id' => array(
+				'type' => 'INT',
+				'constraint' => 11,
+			),
+			'users_id' => array(
+				'type' => 'INT',
+				'constraint' => 11,
+			),
+			'title' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 255,
+			),
+			'created_on' => array(
+				'type' => 'DATETIME',
+				'constraint' => 255,
+			),
+			'modified_on' => array(
+				'type' => 'DATETIME',
+				'constraint' => 255,
+			),
+		));
+
+		$this->dbforge->add_key('id', TRUE);
+
+		$this->dbforge->create_table('yearlevel', TRUE);
+		
+		
+		// we also need to add 'yearlevel_id' to the subject table
+		$fields = array(
+			'yearlevel_id' => array ('type' => 'INT', 'constraint' => 11, ),
+		);
+		$this->dbforge->modify_column('subject',$fields);
+		
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('yearlevel');
+		$this->dbforge->drop_column('subject','yearlevel_id');
+	}
+}
