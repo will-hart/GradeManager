5dd57f0dd480c251b983043ed51393a9c0bc3090
William Hart <hart.wl@gmail.com>
2012-03-30 21:45:54 +0100

    Update display of links in coursework

--- application/views/coursework/view_one.php
+++ application/views/coursework/view_one.php
@@ -2,7 +2,7 @@
 	<?php echo anchor('subject/view/'.$coursework->subject()->id,'<< Back to Subject');?> |
 	<?php if ($coursework->status_id < Model\Status::HANDED_IN) echo anchor('coursework/hand_in/'.$coursework->id,'Hand In') . " | "; ?>  
 	<?php if ($coursework->status_id == Model\Status::HANDED_IN) echo anchor('coursework/enter_score/'.$coursework->id,'Enter Score') . " | "; ?> 
-	<?php if ($coursework->status_id > Model\Status::HANDED_IN) echo anchor('coursework/close/'.$coursework->id,'Close Coursework') . " | "; ?> 
+	<?php if ($coursework->status_id == Model\Status::RETURNED) echo anchor('coursework/close/'.$coursework->id,'Close Coursework') . " | "; ?> 
 	<?php echo anchor('coursework/edit/'.$coursework->id,'EDIT'); ?> | 
 	<?php echo anchor('coursework/delete/'.$coursework->id,'DELETE'); ?>
 </div>
