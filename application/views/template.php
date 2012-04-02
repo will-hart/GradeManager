083970c449bfcd8ebfad71089046a4e626a064eb
mecharius <hart.wl@gmail.com>
2012-04-02 18:43:47 +0100

    Update template to put flashdata in a span-24

--- application/views/template.php
+++ application/views/template.php
@@ -37,13 +37,13 @@
 		</div>
 		
 		<?php if($this->session->flashdata('error')) : ?>
-		<div class="error"><?php echo $this->session->flashdata('error'); ?></div>
+		<div class="error span-24 last"><?php echo $this->session->flashdata('error'); ?></div>
 		<?php endif; ?>
 		<?php if($this->session->flashdata('notice')) : ?>
-		<div class="notice"><?php echo $this->session->flashdata('notice'); ?></div>
+		<div class="notice span-24 last"><?php echo $this->session->flashdata('notice'); ?></div>
 		<?php endif; ?>
 		<?php if($this->session->flashdata('success')) : ?>
-		<div class="success"><?php echo $this->session->flashdata('success'); ?></div>
+		<div class="success span-24 last"><?php echo $this->session->flashdata('success'); ?></div>
 		<?php endif; ?>
 		
 		
