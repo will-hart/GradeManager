db7a1fbf17c3d921a82407305f1b5113bbdae5b2
William Hart <hart.wl@gmail.com>
2012-04-12 11:37:10 +0100

    Rename manage controller class

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit ("No direct script access allowed!");
 
 	
-	class Reports extends CI_Controller {
+	class Manage extends CI_Controller {
 			
 		
 		public function __construct()
