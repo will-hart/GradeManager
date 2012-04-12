0ae49376e20437f0b44bee67fe61aeb2a4cbe772
mecharius <hart.wl@gmail.com>
2012-04-12 15:55:13 +0100

    Add splash logo to register page

--- application/views/auth/pages/register.php
+++ application/views/auth/pages/register.php
@@ -1,32 +1,32 @@
 <div id="login">
-	
+	<?php 
+		$img['src'] = 'assets/images/gm_large_logo.png';
+		$img['alt'] = "Grade Boss Logo";
+		$img['height'] = '332';
+		$img['width'] = '335';
+		$img['class'] = 'large_login_logo';
+		echo img($img);
+	?>
+
+	<form method="post">
 	<?php if(empty($username)) { ?>
-	<h2>Register</h2>
-	<?php } else { ?>
-	<h2>Update</h2>
+	Username:<br />
+	<input type="text" name="username" size="50" class="form" value="<?php echo set_value('username'); ?>" /><br /><?php echo form_error('username', '<div class="error">','</div>'); ?><br />
+	Password:<br />
+	<input type="password" name="password" size="50" class="form" value="<?php echo set_value('password'); ?>" /><?php echo form_error('password', '<div class="error">','</div>'); ?><br /><br />
+	Password confirmation:<br />
+	<input type="password" name="password_conf" size="50" class="form" value="<?php echo set_value('conf_password'); ?>" /><?php echo form_error('conf_password', '<div class="error">','</div>'); ?><br /><br />
 	<?php } ?>
+	Email:<br />
+	<?php if(empty($username)){ ?>
+		<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email'); ?>" /><?php echo form_error('email', '<div class="error">','</div>'); ?><br /><br />
+	<?php }else{ ?>
+	<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email', $email); ?>" /><?php echo form_error('email', '<div class="error">','</div>'); ?><br /><br />
 	
-	<div class="box">
-			<form method="post">
-			<?php if(empty($username)) { ?>
-			Username:<br />
-			<input type="text" name="username" size="50" class="form" value="<?php echo set_value('username'); ?>" /><br /><?php echo form_error('username', '<div class="error">','</div>'); ?><br />
-			Password:<br />
-			<input type="password" name="password" size="50" class="form" value="<?php echo set_value('password'); ?>" /><?php echo form_error('password', '<div class="error">','</div>'); ?><br /><br />
-			Password confirmation:<br />
-			<input type="password" name="password_conf" size="50" class="form" value="<?php echo set_value('conf_password'); ?>" /><?php echo form_error('conf_password', '<div class="error">','</div>'); ?><br /><br />
-			<?php } ?>
-			Email:<br />
-			<?php if(empty($username)){ ?>
-				<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email'); ?>" /><?php echo form_error('email', '<div class="error">','</div>'); ?><br /><br />
-			<?php }else{ ?>
-			<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email', $email); ?>" /><?php echo form_error('email', '<div class="error">','</div>'); ?><br /><br />
-			
-			<?php } if(empty($username)) { ?>
-			<input type="submit" value="Register" name="register" />
-			<?php } else { ?>
-			<input type="submit" value="Update" name="register" />
-			<?php } ?>
-			</form>
-	</div>
+	<?php } if(empty($username)) { ?>
+	<input type="submit" value="Register" name="register" />
+	<?php } else { ?>
+	<input type="submit" value="Update" name="register" />
+	<?php } ?>
+	</form>
 </div>
