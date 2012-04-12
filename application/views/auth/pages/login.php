d324ba08d91c46de1ad9998594124784c8e858c8
mecharius <hart.wl@gmail.com>
2012-04-12 02:25:34 +0100

    Show auth login errors in error divs

--- application/views/auth/pages/login.php
+++ application/views/auth/pages/login.php
@@ -4,9 +4,9 @@
 	<div class="box">
 			<form method="POST">
 			Username/Email:<br />
-			<input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" class="form" /><?php echo form_error('username'); ?><br /><br />
+			<input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" class="form" /><?php echo form_error('username', '<div class="error">','</div>'); ?><br /><br />
 			Password:<br />
-			<input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" class="form" /><?php echo form_error('password'); ?><br /><br />
+			<input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" class="form" /><?php echo form_error('password', '<div class="error">', '</div>'); ?><br /><br />
 			<input type="submit" value="Login" name="login" />
 			</form>
 	</div>
--- application/views/auth/pages/register.php
+++ application/views/auth/pages/register.php
@@ -10,17 +10,17 @@
 			<form method="post">
 			<?php if(empty($username)) { ?>
 			Username:<br />
-			<input type="text" name="username" size="50" class="form" value="<?php echo set_value('username'); ?>" /><br /><?php echo form_error('username'); ?><br />
+			<input type="text" name="username" size="50" class="form" value="<?php echo set_value('username'); ?>" /><br /><?php echo form_error('username', '<div class="error">','</div>'); ?><br />
 			Password:<br />
-			<input type="password" name="password" size="50" class="form" value="<?php echo set_value('password'); ?>" /><?php echo form_error('password'); ?><br /><br />
+			<input type="password" name="password" size="50" class="form" value="<?php echo set_value('password'); ?>" /><?php echo form_error('password', '<div class="error">','</div>'); ?><br /><br />
 			Password confirmation:<br />
-			<input type="password" name="password_conf" size="50" class="form" value="<?php echo set_value('conf_password'); ?>" /><?php echo form_error('conf_password'); ?><br /><br />
+			<input type="password" name="password_conf" size="50" class="form" value="<?php echo set_value('conf_password'); ?>" /><?php echo form_error('conf_password', '<div class="error">','</div>'); ?><br /><br />
 			<?php } ?>
 			Email:<br />
 			<?php if(empty($username)){ ?>
-				<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email'); ?>" /><?php echo form_error('email'); ?><br /><br />
+				<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email'); ?>" /><?php echo form_error('email', '<div class="error">','</div>'); ?><br /><br />
 			<?php }else{ ?>
-			<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email', $email); ?>" /><?php echo form_error('email'); ?><br /><br />
+			<input type="text" name="email" size="50" class="form" value="<?php echo set_value('email', $email); ?>" /><?php echo form_error('email', '<div class="error">','</div>'); ?><br /><br />
 			
 			<?php } if(empty($username)) { ?>
 			<input type="submit" value="Register" name="register" />
