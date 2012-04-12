2b153caefdf6a28a39b2c376a2a3e6f6f9ba122c
mecharius <hart.wl@gmail.com>
2012-04-12 14:54:49 +0100

    Add register link to login page

--- application/views/auth/pages/login.php
+++ application/views/auth/pages/login.php
@@ -3,6 +3,6 @@ Username/Email:<br />
 <input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" /><?php echo form_error('username', '<div class="error">','</div>'); ?><br /><br />
 Password:<br />
 <input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" /><?php echo form_error('password', '<div class="error">', '</div>'); ?><br /><br />
-<input type="submit" value="Login" name="login" />
+<input type="submit" value="Login" name="login" /> or <?php echo anchor('register','Register'); ?>
 </form>
 
