a6108c3fd969fac74832cee655e26fb7b892f794
mecharius <hart.wl@gmail.com>
2012-04-13 18:21:49 +0100

    Reinstate database config file

--- application/config/database.php
+++ application/config/database.php
@@ -49,12 +49,12 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'grades_dba';
+$db['default']['password'] = 'pa55word';
+$db['default']['database'] = 'grades';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
@@ -63,6 +63,7 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+$db['default']['failover'] = array();
 
 
 /* End of file database.php */
