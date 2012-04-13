227c8ef55bd566083cf5b9c466b9ce8351e5aae7
mecharius <hart.wl@gmail.com>
2012-04-13 09:21:01 +0100

    Add unsubscribe views for managing email alerts

---
+++ application/views/profile/unsubscribe_error.php
@@ -0,0 +1,12 @@
+<p>
+There was an error unsubscribing you from email alerts! You can remove 
+alerts at any time by logging on to your profile and deselecting the 
+"allow emails" option.  If you are having difficulties then please feel 
+free to contact us using the links below
+</p>
+
+<p>
+Thanks,
+<br>
+The GradeKeep Team
+</p>
---
+++ application/views/profile/unsubscribe_success.php
@@ -0,0 +1,13 @@
+<p>
+You have been unsubscribed from email alerts.  If this was a mistake you
+can turn them on again by logging in and selecting "allow emails" on your 
+profile screen.  Note if you actually would like to delete your account 
+you can do this by logging in and finding the "delete profile" link at 
+the bottom of your profile screen.
+</p>
+
+<p>
+Thanks,
+<br>
+The GradeKeep Team
+</p>
