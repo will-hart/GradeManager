f25dc319543a60b690d37e2e60c5fb9f63c1a887
mecharius <hart.wl@gmail.com>
2012-04-15 19:02:07 +0100

    Add activation views to auth

---
+++ application/views/auth/activation_success.php
@@ -0,0 +1,12 @@
+<p>
+	You have successfully activated your account! 
+</p>
+<p>
+  You can now <?php echo anchor('login','login to your dashboard'; ?>.
+</p>
+
+<p>
+Thanks,
+<br>
+The GradeKeep Team
+</p>
