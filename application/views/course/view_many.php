b36563a321aaf1bef3540fd2db5882ec1ac843b2
mecharius <hart.wl@gmail.com>
2012-04-15 17:50:29 +0100

    Update course view with improved default handling

--- application/views/course/view_many.php
+++ application/views/course/view_many.php
@@ -10,12 +10,25 @@
 		<tbody>
 	<?php foreach($courses as $c) : ?>
 			<tr>
-				<td><?php echo anchor('course/view/'.$c->id,$c->title); ?></td>
+				<td>
+					<?php 
+						echo anchor('course/view/'.$c->id,$c->title);
+						if ($c->id === $this->session->userdata('default_course'))
+						{
+							echo ' (default)';
+						}
+					?>
+				</td>
 				<td><?php echo count($c->subject()); ?></td>
 				<td>
 					<?php echo anchor('course/edit/'.$c->id,'EDIT'); ?> | 
-					<?php echo anchor('course/delete/'.$c->id,'DELETE'); ?> | 
-					<?php echo anchor('course/set_default/'.$c->id, 'Set as default'); ?>
+					<?php echo anchor('course/delete/'.$c->id,'DELETE'); ?>  
+					<?php 
+						if ($c->id !== $this->session->userdata('default_course'))
+						{
+							echo '|' . anchor('course/set_default/'.$c->id, 'Set as default');
+						}
+					?>
 				</td>
 			</tr>
 	<?php endforeach; ?>
