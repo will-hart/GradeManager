300ce4b35d241a23e71220ddcaf840d61db13099
mecharius <hart.wl@gmail.com>
2012-04-15 20:36:44 +0100

    Add an email template for password resets

---
+++ application/views/emails/forgot_password.php
@@ -0,0 +1,12 @@
+<p>
+	Oh dear, it looks like your password went missing! Its ok, there are a lot of passwords out there and they can run off from time to time. 
+	To reset your password all you need to do is follow the link below and type in a new one!
+</p>
+<p>
+	(If you did not request a password reset then just ignore this email and we'll pretend we never sent it after a little while)
+</p>
+<p>
+	Here is the link: <br>
+	<?php echo anchor('manage/do_password_reset/'.$token,'RESET YOUR PASSWORD HERE'); ?>
+	<br>
+</p>
