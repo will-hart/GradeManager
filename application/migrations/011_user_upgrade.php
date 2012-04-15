4f938ba538117f5b091a1160de0fc60a2fad7690
mecharius <hart.wl@gmail.com>
2012-04-15 18:41:45 +0100

    Update db migrations for user registration confirmation

---
+++ application/migrations/011_user_upgrade.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Migration_profile_unsubscribe extends CI_Migration {
+
+	public function up()
+	{
+		// add a "alert_sent" tinyint column to DB
+		$fields = array(
+			'registration_token' => array ('type' => 'CHAR', 'constraint' => 64 ),
+			'forgot_pass_token' => array ('type' => 'CHAR', 'constraint' => 64 ),
+		);
+		$this->dbforge->add_column('users',$fields);
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_column('users','registration_token');
+		$this->dbforge->drop_column('users','forgot_pass_token');
+	}
+}
