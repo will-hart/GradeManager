505e69f52f10b3bfd3ef3943370b0d972c565a35
mecharius <hart.wl@gmail.com>
2012-04-15 19:07:17 +0100

    Fix code error in auth activation views

--- application/views/auth/activation_error.php
+++ application/views/auth/activation_error.php
@@ -2,7 +2,7 @@
 	The link you provided was not a valid activation URL. Please check the link and try again.
 </p>
 <p>
-	If you require a new activation link, then <?php echo anchor('manage/resend_activation','you can get one here'; ?>.
+	If you require a new activation link, then <?php echo anchor('manage/resend_activation','you can get one here'); ?>.
 </p>
 
 <p>
--- application/views/auth/activation_success.php
+++ application/views/auth/activation_success.php
@@ -2,7 +2,7 @@
 	You have successfully activated your account! 
 </p>
 <p>
-  You can now <?php echo anchor('login','login to your dashboard'; ?>.
+  You can now <?php echo anchor('login','login to your dashboard'); ?>.
 </p>
 
 <p>
