b6243d2d2fee18ff260afee952113a295a2bcc35
William Hart <hart.wl@gmail.com>
2012-04-15 22:22:29 +0100

    Update migrations for template enhancements

---
+++ application/migrations/012_template_categorisation.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Migration_template_categorisation extends CI_Migration {
+
+	public function up()
+	{
+		// add a "alert_sent" tinyint column to DB
+		$fields = array(
+			'official' => array ('type' => 'INT', 'constraint' => 1, 'default' => '0' ),
+			'is_course' => array ('type' => 'INT', 'constraint' => 1, 'default' => '0'),
+			
+		);
+		$this->dbforge->add_column('template',$fields);
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_column('template','official');
+		$this->dbforge->drop_column('template','is_course');
+	}
+}
