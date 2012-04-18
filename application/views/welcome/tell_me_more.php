f207f53161e17b07ae68e67e5bf015ff50a61082
mecharius <hart.wl@gmail.com>
2012-04-18 10:27:23 +0100

    Skeleton code for 'tell_me_more' view

--- application/views/welcome/tell_me_more.php
+++ application/views/welcome/tell_me_more.php
@@ -1,1 +1,7 @@
-MORE!
+<div>
+	<h1 class="app-title">
+		G<span class="make_smaller">RADE</span>K<span class="make_smaller">EEP - MORE INFORMATION</span>
+	</h1>
+
+	<p class="app-detail-long">Sample</p>
+</div>
