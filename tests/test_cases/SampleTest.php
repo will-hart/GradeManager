813436054e7bf68e2d1d694a05df187c5abc033d
mecharius <hart.wl@gmail.com>
2012-04-18 10:40:34 +0100

    Add sample test cases for travsi

---
+++ tests/test_cases/SampleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Gas ORM Unit Test
+ *
+ * Test case for `save` method
+ *
+ * @package     Gas ORM
+ * @category    Unit Test
+ * @version     2.1.1
+ * @author      Taufan Aditya
+ */
+
+class SampleTest extends PHPUnit_Framework_TestCase {
+	
+	private $abc;
+	
+    public function setUp()
+    {
+		$this->abc = "TEST";
+    }
+
+	public function tearDown()
+	{
+		unset($this->abc);
+	}
+
+    public function testSeeIfItWorks()
+    {
+        // Some basic tests to test testing
+        $this->assertInstanceOf('string', $this->abc);
+        $this->assertEquals('string','TEST');
+        $this->assertFalse($this->abc === "MY TEST");
+    }
+
+}
