0d931a8d2a130cdf3517dab5a57ab009b7c60343
mecharius <hart.wl@gmail.com>
2012-04-18 23:25:54 +0100

    Minor changes to tell_me_more view [CI SKIP]

--- application/views/welcome/tell_me_more.php
+++ application/views/welcome/tell_me_more.php
@@ -1,7 +1,20 @@
 <div>
 	<h1 class="app-title">
-		G<span class="make_smaller">RADE</span>K<span class="make_smaller">EEP - MORE INFORMATION</span>
+		F<span class="make_smaller">OR</span> S<span class="make_smaller">TUDENTS</span>
 	</h1>
 
-	<p class="app-detail-long">Sample</p>
+	<p class="app-detail-long">
+		* Visible alerts or emails when some work is coming up
+		* 
+	</p>
+</div>
+
+<div>
+	<h1 class="app-title">
+		F<span class="make_smaller">OR</span> E<span class="make_smaller">DUCATORS</span>
+	</h1>
+
+	<p class="app-detail-long">
+		* Set up course templates that your students can download
+	</p>
 </div>
