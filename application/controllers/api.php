d80a4a3b1fc92d691322cbc9dcc5bb23e23f5866
Mecharius <hart.wl@gmail.com>
2012-04-20 12:55:25 +0100

    Add api controller

---
+++ application/controllers/api.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH.'/core/REST_Controller.php';
+
+class Index extends REST_Controller
+{
+	function user_get()
+	{
+		exit("Got users");
+	}
+}
