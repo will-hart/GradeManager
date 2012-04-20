c491fea2ea5830cfa4b22dc233334856e8e02437
Mecharius <hart.wl@gmail.com>
2012-04-20 13:17:26 +0100

    Update user and coursework getting apis

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -5,8 +5,49 @@ require APPPATH.'/core/REST_Controller.php';
 
 class Api extends REST_Controller
 {
+	
+	/*
+	 * Gets either a single user or a list of users.
+	 * A single user is obtained if an id is passed in get variables
+	 */
 	function user_get()
 	{
-		exit("Got users");
+		
+		$data = NULL;
+		
+		// if an id was passed only return one record
+		if ($this->get('id'))
+		{
+			$data = Gas\User::find($this->get('id'));
+		}
+		else {
+			$data = Gas\User::all();
+		}
+		
+		// now return the response
+		$this->response($data, 200);
+	}
+	
+		
+	/*
+	 * Gets either a single user or a list of users.
+	 * A single user is obtained if an id is passed in get variables
+	 */
+	function coursework_get()
+	{
+		
+		$data = NULL;
+		
+		// if an id was passed only return one record
+		if ($this->get('id'))
+		{
+			$data = Gas\Coursework::find($this->get('id'));
+		}
+		else {
+			$data = Gas\Coursework::all();
+		}
+		
+		// now return the response
+		$this->response($data, 200);
 	}
 }
