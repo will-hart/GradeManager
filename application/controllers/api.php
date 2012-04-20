8d4564a3be986327fd9fec4be10a22816c4f4727
mecharius <hart.wl@gmail.com>
2012-04-20 13:57:44 +0100

    Add subject_get to apis

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -35,8 +35,8 @@ class Api extends REST_Controller
 	
 		
 	/*
-	 * Gets either a single user or a list of users.
-	 * A single user is obtained if an id is passed in get variables
+	 * Gets either a single coursework or a list of courseworks.
+	 * A single coursework is obtained if an id is passed in get variables
 	 */
 	function coursework_get()
 	{
@@ -54,6 +54,34 @@ class Api extends REST_Controller
 		else {
 			$data = $this->db
 				->get('coursework')
+				->result();
+		}
+		
+		// now return the response
+		$this->response($data, 200);
+	}
+	
+			
+	/*
+	 * Gets either a single subject or a list of subjects.
+	 * A single subject is obtained if an id is passed in get variables
+	 */
+	function subject_get()
+	{
+		
+		$data = NULL;
+		
+		// if an id was passed only return one record
+		if ($this->get('id'))
+		{
+			$data = $this->db
+				->where(array('id'=>$this->get('id')))
+				->get('subject')
+				->result();
+		}
+		else {
+			$data = $this->db
+				->get('subject')
 				->result();
 		}
 		
