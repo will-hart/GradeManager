60853e7dafee3d40eb21712d21445dcf241f626c
mecharius <hart.wl@gmail.com>
2012-04-27 16:32:34 +0100

    Style database error page - closes #44

--- application/errors/error_db.php
+++ application/errors/error_db.php
@@ -1,5 +1,33 @@
 <?php 
 $error_type = "Database Error";
-$error_message = "<h1>$heading</h1> <p>$message</p>";
+$error_message = <<<CODE
+
+	<div class="span-16">
+		<img src="../assets/images/knight_slip.png" alt="Error - Knight falling over" /></div>
+
+		<div class="span-8 last">
+			<h1>Oopsie!</h1>
+			<p>
+				It seemed there was a minor error accessing our library.
+				Click back and try again and if all else fails report an issue on 
+				the support forum linked in the footer.  
+			</p>
+			<p>
+				To help out if you are posting on the forum, post a brief
+				description of what you were doing before the error occured 
+				and then add the "techy stuff" below in to your support post.
+			</p>
+			<p class="notice">
+				The Techy stuff: <br> 
+				<span class="fancy">
+					A database error occurred - <br>
+CODE;
+$error_message .= str_replace("<p>", "", str_replace("</p>", "<br>", $message));
+$error_message .= <<<CODE
+				</span>
+			</p>
+		</div>
+CODE;
+
 
 include_once('error_template.php');
