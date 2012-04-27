bd2beb442ec42e09d1057539b1a5fb9fdc13731d
mecharius <hart.wl@gmail.com>
2012-04-27 14:13:11 +0100

    Update 404 error styling

--- application/views/errors/error_404.php
+++ application/views/errors/error_404.php
@@ -1,6 +1,17 @@
-<div class="span-24">
-	<p style="float:left;"><?php echo img('assets/images/GradeKeepShieldKnight'); ?></p>
-	<h1 style="padding-top: 150px;">None Shall Pass!!</h1>
-	<p>You have attempted to click on a link that doesn't exist! Click back and try again and if all else fails report an issue on the support forum (link below).</p>
-	<p class="box_notice">Techy stuff: <br> <span class="fancy">A 404 error occurred trying to access the URL "<?php echo $this->uri->uri_string(); ?>"</span></p>
+<div class="span-16">
+	<?php echo img('assets/images/GradeKeepShieldKnight'); ?>
+</div>
+
+<div class="span-8 last">
+	<h1>None Shall Pass!!</h1>
+	<p>
+		You have attempted to click on a link that doesn't exist!
+	</p>
+	<p>
+		Click back and try again and if all else fails report an issue on 
+		the support forum linked in the footer.  To help out, paste in a
+		brief description of what you were doing before the error occured 
+		and then add the "techy stuff" in your support post.
+	</p>
+	<p class="notice">The Techy stuff: <br> <span class="fancy">A 404 error occurred trying to access the URL "<?php echo $this->uri->uri_string(); ?>"</span></p>
 </div>
