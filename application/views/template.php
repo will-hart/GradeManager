235fdb81ea17debf3225f1c9b9f1ae059dbbf477
Will Hart <hart.wl@gmail.com>
2012-09-25 23:37:29 +0100

    Update headers for v1.2

--- application/views/template.php
+++ application/views/template.php
@@ -58,15 +58,7 @@
 		</div>
 		
 		<div id="footer">		
-			<p>GradeKeep 1.1 is open source software by William Hart.  You can find out more by visiting <a href="http://www.williamhart.info/software/gradekeep">http://www.williamhart.info/software/gradekeep</a>.</p>
-
-			If you found GradeKeep useful, please consider supporting it!
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="92UZ5RC7NJUVS">
-				<input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online.">
-				<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-			</form>
+			<p>GradeKeep 1.2 is open source software by William Hart.  You can find out more by visiting <a href="http://www.williamhart.info/software/gradekeep">http://www.williamhart.info/blog/2012/04/gradekeep-coursework-management</a>.</p>
 		</div>
 	
 	</body>
